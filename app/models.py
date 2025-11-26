38987e734fc2e6e4a47d1ec80a9566738fb87e86
Soham0798 <sohambz.patwardhan@gmail.com>
2025-11-26 11:47:20 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -31,5 +31,6 @@ class Bus(Base):
     route = Column(String)
     latitude = Column(Float)   # 🆕 Current latitude
     longitude = Column(Float)  # 🆕 Current longitude
-     capacity = Column(Integer, default=40)
+    capacity = Column(Integer, default=40)
     booked_seats = Column(Integer, default=0) 
+
