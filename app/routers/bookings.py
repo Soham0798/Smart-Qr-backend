dd39857825d6f4046234ab76134ae9ab4d05c47b
Soham0798 <sohambz.patwardhan@gmail.com>
2025-11-27 09:58:03 +0530

    Update bookings.py

--- app/routers/bookings.py
+++ app/routers/bookings.py
@@ -70,7 +70,7 @@ def create_booking(
     qr_folder = os.path.join(os.getcwd(), "app", "qrs")
     os.makedirs(qr_folder, exist_ok=True)
 
-    frontend_domain = "https://smart-qr-frontend-i8cu-8e0rxo357-soham0798s-projects.vercel.app"
+    frontend_domain = "https://smart-qr-frontend-i8cu.vercel.app"
     qr_redirect_url = f"{frontend_domain}/tickets/{db_booking.id}"
     
     # Generate QR image
@@ -116,6 +116,7 @@ def get_public_booking(booking_id: int, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[schemas.BookingOut])
 def get_user_bookings(db: Session = Depends(get_db), user=Depends(get_current_user)):
     return db.query(models.Booking).filter(models.Booking.user_id == user.id).all()
+
 
 
 
